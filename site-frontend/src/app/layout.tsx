5d7c859df506d85d8f2d6328b09f1882a2177015
Roy Vivat <royvivat@gmail.com>
2024-03-01 23:37:35 -0800

    removed more antd

--- site-frontend/src/app/layout.tsx
+++ site-frontend/src/app/layout.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
 
-import StyledComponentsRegistry from '../lib/AntdRegistry';
-
 import './globals.css';
-import Navbar from '@/components/navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
     <body className={inter.className}>
-      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      
     </body>
   </html>
 );
--- site-frontend/src/app/page.tsx
+++ site-frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
-import Navbar from '@/components/navbar';
-
+import React, { useState } from 'react';;
 
 export default function Home() {
 	// if (typeof window !== "undefined") {
