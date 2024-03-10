12bef9334fefb5bf9fa3dc2634b726b222603a16
Roy Vivat <royvivat@gmail.com>
2024-03-10 01:18:43 -0800

    added favicon

--- royvivat-dev-nextjs/src/app/layout.js
+++ royvivat-dev-nextjs/src/app/layout.js
@@ -1,11 +1,20 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html className='scroll-smooth' style={{scrollBehavior:'smooth'}} data-theme="cmyk" lang="en">
+    <html
+      className="scroll-smooth"
+      style={{ scrollBehavior: "smooth" }}
+      data-theme="cmyk"
+      lang="en"
+    >
+      <head>
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- royvivat-dev-nextjs/src/app/page.js
+++ royvivat-dev-nextjs/src/app/page.js
@@ -1,14 +1,16 @@
 import Hero from "./components/Hero";
-import AboutMe from "./components/AboutMe"
-import Projects from "./components/Projects"
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div style={{backgroundColor: "#f2f8fd"}}>
-      <Hero/>
-      <div className="m-20"/>
-      <AboutMe/>
-      <div className="m-20"/>
-      <Projects/>
+    <div style={{ backgroundColor: "#f2f8fd" }}>
+      <Hero className="ml-[20px] mr-[20px]" />
+      <div className="m-20" />
+      <AboutMe />
+      <div className="m-20" />
+      <Projects />
     </div>
   );
 }
